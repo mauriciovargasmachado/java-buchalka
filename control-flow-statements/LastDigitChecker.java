a86fc881b4aca3394eb3a72dae3c019eda2eda7d
MAURICIO VARGAS <mauriciovargasmachado@gmail.com>
2023-06-20 15:34:08 -0400

    Update LastDigitChecker.java
    You forgot about the second method requested!.

--- control-flow-statements/LastDigitChecker.java
+++ control-flow-statements/LastDigitChecker.java
@@ -1,17 +1,29 @@
 public class LastDigitChecker {
 
-    public static void main(String[] args) {
-        System.out.println(hasSameLastDigit(41, 22, 71));
-        System.out.println(hasSameLastDigit(231, 372, 423));
-        System.out.println(hasSameLastDigit(91, 991, 532));
+   public static void main(String [] args){
+
+        System.out.println(hasSameLastDigit (41,22,71));
+        System.out.println(isValid (10));
+
     }
 
-    public static boolean hasSameLastDigit (int firstNumber, int secondNumber, int thirdNumber) {
-        if ((firstNumber >= 10 && firstNumber <= 1000) && (secondNumber >= 10 && secondNumber <= 1000) && (thirdNumber >= 10 && thirdNumber <= 1000)) {
-            System.out.println("Do any of given numbers (" + firstNumber + ", " + secondNumber + ", " + thirdNumber + ") share a last digit?");
-            return ((firstNumber % 10 == secondNumber % 10) || (firstNumber % 10 == thirdNumber % 10) || (secondNumber % 10 == thirdNumber % 10));
+    public static boolean hasSameLastDigit(int fDigit, int sDigit, int tDigit) {
+
+        if((isValid(fDigit) && isValid(sDigit) && isValid(tDigit))) {
+
+            if(fDigit % 10 == sDigit % 10 || sDigit % 10 == tDigit % 10 || fDigit % 10== tDigit % 10) {
+
+                return true;
+
+            }
+
         }
-        System.out.println("Invalid input");
         return false;
+
+    }
+    public static boolean isValid(int number) {
+
+        return(number >= 10 && number <= 1000);
+
     }
 }
